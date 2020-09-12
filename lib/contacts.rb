b15313686512d751a467307157cbf3caf268c46c
Dasha Lary <dk2800@nyu.edu>
2020-09-12 20:48:30 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ require 'pry'
   }
   
 def remove_strawberry(contacts)
-contacts.each do |attribute, value|
+contacts["Freddy Mercury"].each do |attribute, value|
   if attribute == :favorite_ice_cream_flavors
 
 end
